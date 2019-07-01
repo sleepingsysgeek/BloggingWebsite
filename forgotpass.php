54b6fd169b1bd9e4190929759817c8b58d63f99c
Praveen Mishra <praveenmishraemail@gmail.com>
2019-07-01 17:38:59 +0530

    Update forgotpass.php

--- forgotpass.php
+++ forgotpass.php
@@ -47,9 +47,9 @@
 					}
 				}
 				if($error == 0){
-					$server = "localhost";
-					$username = "joker";
-					$password = "djoker";
+					$server = "10.5.0.63";
+					$username = "root";
+					$password = "passwd@123";
 					$db = "project";
 					$conn = new mysqli($server,$username,$password,$db);
 					if($conn->connect_error){
